e4bc448e2534a4815771ca715da4ce5cdf7f9bc4
tpaiwand <103455613+tpaiwand@users.noreply.github.com>
2022-05-22 23:10:21 +0200

    Add files via upload

--- atm02.py
+++ atm02.py
@@ -1,16 +1,22 @@
 
-print("Welcome to the DeDI Bank")
+print("Welcome to the Example Bank")
 user_pin = 12345
-#user_pin = int(input({12345, 12346, 12347,}))
+accounts = {"ABC": 12345, "ABCD": 12346, "ABCDE": 12347}
 trials = 3
-balance = 50000
+balance = int(50000)
+account_valid = False
 
 while trials != 0:
-    pin = int(input("Please enter your 5 digit PIN number: "))
-    if pin != user_pin:
-        trials -= 1
-        print("Wrong PIN number, you have", trials, "trials left")
-             
+    input_account_name = str(input("Please enter your name: "))
+    if (input_account_name in accounts):
+        account_valid = True
+    else:
+        print("No valid account found.")
+        exit(1)
+    input_pin = int(input("Please insert your 5 digit pin number!"))
+    if input_pin != accounts [input_account_name]:
+            trials -= 1
+            print("Wrong PIN number, you have", trials, "trials left")
     else:
         user_choice = input("D: deposit, W: withdraw, B: balance  :   ")
         if user_choice == "D":
@@ -29,10 +35,24 @@ while trials != 0:
         
     user_exit = input("Would you like to withdraw from your account? Y/N: ")
     if user_exit == "N":
-        print("Thank you for using the DeDI Bank")
-        break
+        user_choice = input("D: deposit, W: withdraw, B: balance  :   ")
+        if user_choice == "D":
+            user_deposit = int(input("Please enter the amount you want to deposit: "))
+            total_balance = user_deposit + balance
+            print(user_deposit, "$ have been deposited and your total balance is $", total_balance)
+        
+        if user_choice == "W":
+            user_withdraw = int(input("Please enter the amount you want to withdraw: "))
+            total_balance = user_withdraw - balance
+            print(user_withdraw, "$ have been withdrawn from your account, current balance is $", total_balance) 
+        
+        if user_choice == "B":
+            user_balance = balance
+            print("Your balance is $", user_balance)
     else:
-        continue
+        print("Thank you for using the Example Bank")
+        break
     
+        continue
       
       
